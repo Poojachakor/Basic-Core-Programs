b68bb11f355a614217f0f083b77cd417dcfde362
Poojachakor <chakorpooja07@gmail.com>
2023-02-22 16:21:52 +0530

    Add: UC7 file

--- BasicCoreProblems/Program.cs
+++ BasicCoreProblems/Program.cs
@@ -4,9 +4,8 @@
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Welcome To Basic Core Programs");
-            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:Powerof2\n4:HarmonicNumber\n5:PrimeFactors\n6:RemainderAndQuotient\n11:Exit");
+            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:PowerOfTwo\n4:HarmonicNumber\n5:PrimeFactors\n6:RemainderAndQuotient\n7:SwapTwoNumber\n11:Exit");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -24,10 +23,13 @@
                     HarmonicNumber.HarmonicValue();
                     break;
                 case 5:
-                    PrimeFactors.PrimeFactor();
+                     PrimeFactors.PrimeFactor();
                     break;
                 case 6:
-                    RemainderAndQuotient.Divmethod();
+                     RemainderAndQuotient.Divmethod();
+                    break;
+                case 7:
+                    SwapTwoNumbers.SwapNumbers();
                     break;
                 default:
                     Console.WriteLine("choose the above options");
@@ -37,6 +39,15 @@
         }
 
 
+
     }
 }
+
+
+
+
+    
+
+
+
 
---
+++ BasicCoreProblems/SwapTwoNumbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    internal class SwapTwoNumbers
+    {
+
+        public static void SwapNumbers()
+        {
+            Console.WriteLine("Enter a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            a = a + b; //a=15 (5+10)      
+            b = a - b; //b=5 (15-10)      
+            a = a - b; //a=10 (15-5)   
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+
+}
