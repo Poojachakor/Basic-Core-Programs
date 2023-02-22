dad69c9e0556506d8df725edddc7127e0dbb0acb
Poojachakor <chakorpooja07@gmail.com>
2023-02-22 15:35:55 +0530

    Add: UC6 file

--- BasicCoreProblems/Program.cs
+++ BasicCoreProblems/Program.cs
@@ -6,7 +6,7 @@
         {
 
             Console.WriteLine("Welcome To Basic Core Programs");
-            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:Powerof2\n4:HarmonicNumber\n5:PrimeFactors\n11:Exit");
+            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:Powerof2\n4:HarmonicNumber\n5:PrimeFactors\n6:RemainderAndQuotient\n11:Exit");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -25,6 +25,9 @@
                     break;
                 case 5:
                     PrimeFactors.PrimeFactor();
+                    break;
+                case 6:
+                    RemainderAndQuotient.Divmethod();
                     break;
                 default:
                     Console.WriteLine("choose the above options");
