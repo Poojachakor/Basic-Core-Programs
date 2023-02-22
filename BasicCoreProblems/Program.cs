cfdb552353badfae844a935b86d9d22fee318b9f
Poojachakor <chakorpooja07@gmail.com>
2023-02-22 16:52:38 +0530

    Add: UC10 file

--- BasicCoreProblems/Program.cs
+++ BasicCoreProblems/Program.cs
@@ -4,9 +4,8 @@
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Welcome To Basic Core Programs");
-            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:Powerof2\n4:HarmonicNumber\n5:PrimeFactors\\n6:RemainderAndQuotient\n7:SwapTwoNumbers\n8:EvenorOdd\n9:VowelOrConsonant\n11:Exit");
+            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:Powerof2\n4:HarmonicNumber\n5:PrimeFactors\n6:RemainderAndQuotient\n7:SwapTwoNumbers\n8:EvenorOdd\n9:VowelOrConsonant\n10:LargestAmongThreeNumbers\n11:Exit");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -24,10 +23,10 @@
                     HarmonicNumber.HarmonicValue();
                     break;
                 case 5:
-                   PrimeFactors.PrimeFactor();
+                    PrimeFactors.PrimeFactor();
                     break;
                 case 6:
-                   RemainderAndQuotient.Divmethod();
+                    RemainderAndQuotient.Divmethod();
                     break;
                 case 7:
                     SwapTwoNumbers.SwapNumbers();
@@ -38,11 +37,15 @@
                 case 9:
                     VowelOrConsonant.VowelOrConsonantCheck();
                     break;
+                case 10:
+                    LargestAmongThreeNumbers.FindingLargeNum();
+                    break;
                 default:
                     Console.WriteLine("choose the above options");
                     break;
             }
             Console.ReadLine();
+
         }
 
 
