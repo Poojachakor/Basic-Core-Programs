99155f47a995f84eceef658f4f8d2a627d7251f6
Poojachakor <chakorpooja07@gmail.com>
2023-02-22 16:44:30 +0530

    Add: UC9 file

--- BasicCoreProblems/Program.cs
+++ BasicCoreProblems/Program.cs
@@ -6,7 +6,7 @@
         {
 
             Console.WriteLine("Welcome To Basic Core Programs");
-            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:Powerof2\n4:HarmonicNumber\n5:PrimeFactors\n6:RemainderAndQuotient\n7:SwapTwoNumber\n8:EvenOrOdd\n11:Exit");
+            Console.WriteLine("\n1:HeadAndTail\n2:LeapYear\n3:Powerof2\n4:HarmonicNumber\n5:PrimeFactors\\n6:RemainderAndQuotient\n7:SwapTwoNumbers\n8:EvenorOdd\n9:VowelOrConsonant\n11:Exit");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -24,16 +24,19 @@
                     HarmonicNumber.HarmonicValue();
                     break;
                 case 5:
-                  PrimeFactors.PrimeFactor();
+                   PrimeFactors.PrimeFactor();
                     break;
                 case 6:
-                    RemainderAndQuotient.Divmethod();
+                   RemainderAndQuotient.Divmethod();
                     break;
                 case 7:
                     SwapTwoNumbers.SwapNumbers();
                     break;
                 case 8:
                     EvenorOdd.EvenOrOddCheck();
+                    break;
+                case 9:
+                    VowelOrConsonant.VowelOrConsonantCheck();
                     break;
                 default:
                     Console.WriteLine("choose the above options");
