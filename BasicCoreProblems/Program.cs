4e50f94e705dd155a1d90119d07dd3ec258c1fed
Poojachakor <chakorpooja07@gmail.com>
2023-02-22 14:08:55 +0530

    Add: UC1 file

---
+++ BasicCoreProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicCoreProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Basic Core Programs");
+            Console.WriteLine("Enter the number of times you want to flip the coin");
+            int n = Convert.ToInt32(Console.ReadLine());
+            HeadAndTail.HeadandTailPercentage(n);
+            Console.ReadLine();
+        }
+    }
+}
